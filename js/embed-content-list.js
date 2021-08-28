62218f41c92602eeb75b30a5dde761d63492742b
mtakamur <masahiro.takamura.alice@gmail.com>
2021-08-28 12:46:45 +0900

    WIP: implementing contents list js

--- js/embed-content-list.js
+++ js/embed-content-list.js
@@ -1,17 +1,28 @@
 /**
  * A script to generate contents list based on /js/json/content_list.json
  */
-const JSON_PATH = "/js/json/content_list.json"
-document.write(makeContent());
+const JSON_PATH = "/js/json/content_list.json";
+fetchJson().then((data) => makeContents(data.contents));
+  //document.write('<li><a href="/cpp/top.html">C/C++</a></li>');
 
-function makeContent() {
-    return '<li><a href="/index.html">generation test</a></li>'
+function makeContents(contentsJsonObject) {
+  for (let i = 0; i < contentsJsonObject.length; i++) {
+      let data = contentsJsonObject[i]
+      let embedString = makeEmbedString(data.title, data.url)
+      console.log(embedString)
+      writeToDocument(embedString)
+  }
 }
 
-function embedContentList() {
-    console.log("test")
+function fetchJson() {
+  return fetch(JSON_PATH).then((response) => response.json());
 }
 
-function parseJson() {
-    let contentsList = 0
+function makeEmbedString(title, url) {
+  return '<li><a href="' + url + '">' + title + "</a></li>";
+}
+
+function writeToDocument(content) {
+    let contentElements = document.getElementById("contents");
+    contentElements.insertAdjacentHTML("afterend", content)
 }

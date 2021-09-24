0483d6cc924a0aaed1f432cba037c1f3c02d5e31
mtakamur <masahiro.takamura.alice@gmail.com>
2021-09-24 18:02:32 +0900

    WIP: Implementing script

--- js/embed-date.js
+++ js/embed-date.js
@@ -2,26 +2,40 @@
  * A script to embed article date based on /js/json/content_list.json
  */
 
-const JSON_PATH = "/js/json/articles.json";
-const TARGET_ID = "history";
-const EMBEDDING_TARGET_ID = "date";
-fetchJson().then((data) => makeContents(data.history));
+function embedDate(...path) {
+  let JSON_PATH = "/js/json/articles.json";
+  let TARGET_ID = "history";
+  let EMBEDDING_TARGET = "date";
 
-function embedDate() {
   // Input path to content in JSON as arguments.
-  fetchJson().then((data) => getDateFor(data, arguments).then());
+  fetchJson(JSON_PATH)
+  .then((data) => getDateNode(data, path)
+  .then((dateNode) => );
 
-  document
-    .getElementById(EMBEDDING_TARGET_ID)
-    .insertAdjacentHTML("beforeend", "test");
+  // document
+  //   .getElementById(EMBEDDING_TARGET_ID)
+  //   .insertAdjacentHTML("beforeend", "test");
 }
 
 // Functions called in the main sequence.
-function getDateFor(data, path) {
-  let date;
-  for (let i = 0; i < path.length; i++) {
+function getDateNode(data, path) {
+  let dateNode = data;
+
+  // Reach to list of article node.
+  for (let i = 0; i < path.length - 1; i++) {
     let nextNode = path[i];
-    date = data.get(nextNode);
+    dateNode = dateNode[nextNode];
+  }
+
+  // Find article node whose id matches to final parameter of path.
+  dateNode = findArticleById(dateNode, path[path.length - 1]);
+  console.log(dateNode);
+  return dateNode;
+}
+
+function findArticleById(articles, id) {
+  for (let i = 0; i < articles.length; i++) {
+    if (articles[i].id == id) return articles[i];
   }
 }
 
@@ -48,8 +62,8 @@ function makeContents(contentsJsonObject) {
   }
 }
 
-function fetchJson() {
-  return fetch(JSON_PATH).then((response) => response.json());
+function fetchJson(jsonPath) {
+  return fetch(jsonPath).then((response) => response.json());
 }
 
 function makeEmbedString(date, title, url, isNew) {

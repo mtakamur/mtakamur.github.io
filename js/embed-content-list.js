b48899fde44b1e259c931e0fec93e5c7e701eb6c
mtakamur <masahiro.takamura.alice@gmail.com>
2021-09-26 07:32:25 +0900

    Enable date embedding

--- js/embed-content-list.js
+++ js/embed-content-list.js
@@ -1,28 +1,29 @@
 /**
  * A script to embed update history on the top page.
  */
+function embedContentList() {
+  let JSON_PATH = "/js/json/content_list.json";
+  fetchJson().then((data) => makeContents(data.contents));
 
-const JSON_PATH = "/js/json/content_list.json";
-fetchJson().then((data) => makeContents(data.contents));
-
-// Functions called in the main sequence.
-function makeContents(contentsJsonObject) {
-  for (let i = 0; i < contentsJsonObject.length; i++) {
-      let data = contentsJsonObject[i]
-      let embedString = makeEmbedString(data.title, data.url)
-      writeToDocument(embedString)
+  // Functions called in the main sequence.
+  function makeContents(contentsJsonObject) {
+    for (let i = 0; i < contentsJsonObject.length; i++) {
+      let data = contentsJsonObject[i];
+      let embedString = makeEmbedString(data.title, data.url);
+      writeToDocument(embedString);
+    }
   }
-}
 
-function fetchJson() {
-  return fetch(JSON_PATH).then((response) => response.json());
-}
+  function fetchJson() {
+    return fetch(JSON_PATH).then((response) => response.json());
+  }
 
-function makeEmbedString(title, url) {
-  return '<li><a href="' + url + '">' + title + "</a></li>";
-}
+  function makeEmbedString(title, url) {
+    return '<li><a href="' + url + '">' + title + "</a></li>";
+  }
 
-function writeToDocument(content) {
+  function writeToDocument(content) {
     let contentElements = document.getElementById("contents");
-    contentElements.insertAdjacentHTML("beforeend", content)
+    contentElements.insertAdjacentHTML("beforeend", content);
+  }
 }
--- js/embed-date.js
+++ js/embed-date.js
@@ -1,7 +1,6 @@
 /**
  * A script to embed article date based on /js/json/content_list.json
  */
-
 function embedDate(...path) {
   let JSON_PATH = "/js/json/articles.json";
   let EMBEDDING_TARGET = "date";
@@ -14,55 +13,51 @@ function embedDate(...path) {
       writeDateToDocument(EMBEDDING_TARGET, embeddingString)
     );
 
-  // document
-  //   .getElementById(EMBEDDING_TARGET_ID)
-  //   .insertAdjacentHTML("beforeend", "test");
-}
+  // Functions called in the main sequence.
+  function getDateNode(data, path) {
+    let dateNode = data;
 
-// Functions called in the main sequence.
-function getDateNode(data, path) {
-  let dateNode = data;
+    // Reach to list of article node.
+    for (let i = 0; i < path.length - 1; i++) {
+      let nextNode = path[i];
+      dateNode = dateNode[nextNode];
+    }
 
-  // Reach to list of article node.
-  for (let i = 0; i < path.length - 1; i++) {
-    let nextNode = path[i];
-    dateNode = dateNode[nextNode];
+    // Find article node whose id matches to final parameter of path.
+    dateNode = findArticleById(dateNode, path[path.length - 1]);
+    return dateNode;
   }
 
-  // Find article node whose id matches to final parameter of path.
-  dateNode = findArticleById(dateNode, path[path.length - 1]);
-  return dateNode;
-}
-
-function findArticleById(articles, id) {
-  for (let i = 0; i < articles.length; i++) {
-    if (articles[i].id == id) return articles[i];
+  function findArticleById(articles, id) {
+    for (let i = 0; i < articles.length; i++) {
+      if (articles[i].id == id) return articles[i];
+    }
   }
-}
 
-function makeDateString(articleNode) {
-  console.log();
-  let dateNode = articleNode["date"];
-  return makeEmbedString(
-    formatDate(dateNode["year"], dateNode["month"], dateNode["date"])
-  );
-}
+  function makeDateString(articleNode) {
+    console.log();
+    let dateNode = articleNode["date"];
+    return makeEmbedString(
+      formatDate(dateNode["year"], dateNode["month"], dateNode["date"])
+    );
+  }
 
-function fetchJson(jsonPath) {
-  return fetch(jsonPath).then((response) => response.json());
-}
+  function fetchJson(jsonPath) {
+    return fetch(jsonPath).then((response) => response.json());
+  }
 
-function makeEmbedString(dateString) {
-  return "<p> 更新日: " + dateString + "</p>";
-}
+  function makeEmbedString(dateString) {
+    return "<p> 更新日: " + dateString + "</p>";
+  }
 
-function formatDate(year, month, date) {
-  let splitter = "/";
-  return year + splitter + month + splitter + date;
-}
+  function formatDate(year, month, date) {
+    let splitter = "/";
+    return year + splitter + month + splitter + date;
+  }
 
-function writeDateToDocument(embeddingTargetId, content) {
-  document
-    .getElementById(embeddingTargetId)
-    .insertAdjacentHTML("beforeend", content);
+  function writeDateToDocument(embeddingTargetId, content) {
+    document
+      .getElementById(embeddingTargetId)
+      .insertAdjacentHTML("beforeend", content);
+  }
 }

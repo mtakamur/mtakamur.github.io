2cd2c2763e5875d645fef91865a3f66ff13ee075
mtakamur <masahiro.takamura.alice@gmail.com>
2021-10-19 00:55:29 +0900

    WIP: trying to export function

--- js/article-constructor/construct-article.js
+++ js/article-constructor/construct-article.js
@@ -1,13 +1,14 @@
-import constructDate from "/js/article-constructor/construct-date.js";
-import constructHeader from "/js/article-constructor/construct-header.js";
-import constructMenuBar from "/js/article-constructor/construct-menu-bar.js";
-import constructBodyHeader from "/js/article-constructor/construct-body-header.js";
+// import constructDate from "/js/article-constructor/construct-date.js";
+// import constructHeader from "/js/article-constructor/construct-header.js";
+// import constructMenuBar from "/js/article-constructor/construct-menu-bar.js";
+import { constructBodyHeader } from "/js/article-constructor/construct-body-header.js";
 
 /**
  * Script to construct article page (common contents).
  */
 function constructArticle(...path) {
-  constructDate(path);
+  //constructDate(path);
+  constructBodyHeader();
 
   /**
    * Obtain json from given path.
--- js/article-constructor/construct-body-header.js
+++ js/article-constructor/construct-body-header.js
@@ -1,1 +1,16 @@
-export function constructBodyHeader() {}
+function constructBodyHeader() {
+  let containerId = "container";
+  let logoImage = "/images/logo.png";
+  let bodyHeaderTemplate = `
+      <header id="body_header">
+        <div id="logo" style="text-align: center">
+          <a href="/index.html">
+            <img src="${logoImage}" style="width: 150px; height: 46px"
+          /></a>
+        </div>
+      </header>
+  `;
+  document
+    .getElementById(containerId)
+    .insertAdjacentHTML("afterbegin", bodyHeaderTemplate);
+}
